6a1c2befd98d427145ce07bc5fd785107b02fd48
WANDJA TCHATAT Thierry (SNCF / DGA NUMERIQUE / e.SNCF Sol.DEA ASO AUTOM) <8912981Y@commun.ad.sncf.fr>
2023-10-08 20:55:17 +0200

    Correction de la variable locale db_name

--- main.tf
+++ main.tf
@@ -115,5 +115,5 @@ variable "ec2_type" {
 locals {
   elb_name = "nuumfactory-${var.environnement}-lb-${var.digit}"
   ec2_name = "nuumfactory-${var.environnement}-ec2-${var.digit}"
-  db_name  = "nuumfactory-${var.environnement}-elb-${var.digit}"
+  db_name  = "nuumfactory-${var.environnement}-db-${var.digit}"
 }
